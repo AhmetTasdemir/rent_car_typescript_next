2a9e62e2501eec9b8b8c56d9320f3be1884f4714
Ahmet Taşdemir <ahmettsdmr1312@gmail.com>
2023-07-10 15:36:45 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import Footer from "@/components/Footer";
+
+export const metadata = {
+  title: "Car Hub",
+  description: "Discover world's best car showcase application",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className='relative'>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
